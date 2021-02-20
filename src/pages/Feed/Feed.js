28551d0b7e157c124c78310909c2a63bb498a298
yapoey_work <mohamed.medhat92@gmail.com>
2021-02-21 04:12:25 +0900

    Editing image url

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -59,7 +59,12 @@ class Feed extends Component {
             })
             .then(resData => {
                 this.setState({
-                    posts: resData.posts,
+                    posts: resData.posts.map(post => {
+                        return {
+                            ...post,
+                            imagePath: post.imageUrl
+                        }
+                    }),
                     totalPosts: resData.totalItems,
                     postsLoading: false
                 });
@@ -105,22 +110,20 @@ class Feed extends Component {
         this.setState({
             editLoading: true
         });
-        // Set up data (with image!)
+        const formData = new FormData();
+        formData.append('title', postData.title);
+        formData.append('content', postData.content);
+        formData.append('image', postData.image);
         let url = 'http://localhost:8014/feed/post';
         let method = 'POST';
         if (this.state.editPost) {
-            url = 'URL';
+            url = 'http://localhost:8014/feed/post/' + this.state.editPost._id;
+            method = 'PUT'
         }
 
         fetch(url, {
                 method: method,
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    title: postData.title,
-                    content: postData.content
-                })
+                body: formData
             })
             .then(res => {
                 if (res.status !== 200 && res.status !== 201) {
@@ -171,7 +174,9 @@ class Feed extends Component {
 
     deletePostHandler = postId => {
         this.setState({ postsLoading: true });
-        fetch('URL')
+        fetch('http://localhost:8014/feed/post/' + postId, {
+                method: 'DELETE'
+            })
             .then(res => {
                 if (res.status !== 200 && res.status !== 201) {
                     throw new Error('Deleting a post failed!');
